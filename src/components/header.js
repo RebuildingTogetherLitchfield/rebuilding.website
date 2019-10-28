435b2fbb627a53d055a4a717c207e6bfe0664d33
Cipher-Coder <matthew.rebehn@gmail.com>
2019-10-28 17:55:05 -0400

    added some SVG's | Finished Nav | Fixed and Finished Header | Started index page

--- src/components/header.js
+++ src/components/header.js
@@ -5,31 +5,33 @@ import React from "react"
 import logo from "../images/Logo_Stacked_black.png"
 import "../styles/navigation.css"
 
-const Header = ({ siteTitle, menuLinks }) => (
-  <header className="d-none d-md-block header-main">
-    <div className="header-container">
-      <div className="head_nav-left">
-        <a href="#" className="head-nav">
-          <span className="small-word">In The </span>
-          News
-        </a>
+const Header = ({ siteTitle, menuLinks }) => {
+  return (
+    <header className="d-none d-md-block header-main">
+      <div className="header-container">
+        <div className="head_nav-left">
+          <a href="#" className="head-nav">
+            <span className="small-word">In The </span>
+            News
+          </a>
+        </div>
+        <div className="head_nav-right">
+          <a href="#" className="head-nav">
+            <span className="small-word">Get</span>
+            Involved
+          </a>
+        </div>
+        <Link to="/" className="link-logo">
+          <img
+            src={logo}
+            className="header-logo"
+            alt="Rebuilding Together Logo"
+          />
+        </Link>
       </div>
-      <div className="head_nav-right">
-        <a href="#" className="head-nav">
-          <span className="small-word">Get</span>
-          Involved
-        </a>
-      </div>
-      <Link to="/" className="link-logo">
-        <img
-          src={logo}
-          className="header-logo"
-          alt="Rebuilding Together Logo"
-        />
-      </Link>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,30 @@ import Layout from "../components/Layout"
 import Image from "../components/Image"
 import SEO from "../components/Seo"
 
+import "../styles/index.css"
+import construction from "../images/construction.svg"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1 style={{ marginTop: `20px` }}>Hi people</h1>
-
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="container-fluid index-intro">
+      <div className="row">
+        <div className="col-md-6 intro-container">
+          <h1 className="intro-heading">Our Mission:</h1>
+          <h4 className="mission-text center">
+            Repairing homes, revitalizing communities, rebuilding lives.
+          </h4>
+          <h2 className="vision-heading">Our Vision:</h2>
+          <h4 className="vision-text center">
+            Safe homes and communities for everyone.
+          </h4>
+        </div>
+        <div className="col-md-6 intro-image-container">
+          <img className="intro-image" src={construction} />
+        </div>
+      </div>
     </div>
+
     <Link to="/about-us/">Go to About Us</Link>
   </Layout>
 )
