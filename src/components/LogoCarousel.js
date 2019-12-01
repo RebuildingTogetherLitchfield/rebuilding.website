d1540880ede700f9d80b3fd9e16852eb034a2545
Cipher-Coder <matthew.rebehn@gmail.com>
2019-12-01 13:44:24 -0500

    working on logo slider to get rid of jank: The logo slider has a slight jank at the end right before it starts again. I am trying to only use css

--- src/components/LogoCarousel.js
+++ src/components/LogoCarousel.js
@@ -8,16 +8,19 @@ import boaLogo from "../images/logos/boaLogo.jpg"
 import microsoftLogo from "../images/logos/microsoftLogo.jpg"
 import commFoundation from "../images/logos/commFoundation1.jpg"
 import bogues from "../images/logos/bogues1.jpg"
+import BnD from "../images/logos/BnDcontrol.jpg"
+import sentinel from "../images/logos/sentinel.jpg"
+import alliedRoof from "../images/logos/alliedRoof.jpg"
 
 const LogoCarousel = () => {
   return (
-    <div className={`container ${styles.logoCarousel}`}>
+    <div className={`container-fluid ${styles.logoCarousel}`}>
       <div className={styles.slider}>
         <div className={styles.slideTrack}>
           <div className={styles.slide}>
             <img
               src={lowesLogo}
-              alt="Sponsor is Lowes"
+              alt="This Sponsor is Lowes"
               height="100"
               width="250"
             />
@@ -27,63 +30,136 @@ const LogoCarousel = () => {
               src={homeDepot}
               height="100"
               width="250"
-              alt="Sponsor is Home Depot"
+              alt="This Sponsor is Home Depot"
             />
           </div>
           <div className={styles.slide}>
-            <img src={searsLogo} height="100" width="250" alt="" />
+            <img
+              src={searsLogo}
+              height="100"
+              width="250"
+              alt="This Sponsor is Sears"
+            />
           </div>
           <div className={styles.slide}>
             <img
               src={websterBank}
               height="100"
               width="250"
-              alt="Sponsor is Webster Bank"
+              alt="This Sponsor is Webster Bank"
             />
           </div>
           <div className={styles.slide}>
-            <img src={boaLogo} height="100" width="250" alt="" />
+            <img
+              src={boaLogo}
+              height="100"
+              width="250"
+              alt="This Sponsor is Bank of America"
+            />
           </div>
           <div className={styles.slide}>
-            <img src={microsoftLogo} height="100" width="250" alt="" />
+            <img
+              src={microsoftLogo}
+              height="100"
+              width="250"
+              alt="This Sponsor is Microsoft"
+            />
           </div>
           <div className={styles.slide}>
             <img
               src={commFoundation}
               height="100"
               width="250"
-              alt="Sponsor is Community Foundation"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img src={bogues} height="100" width="250" alt="" />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src="https://i.ibb.co/wd7FSnZ/brand-number-ten-1.png"
-              height="100"
-              width="250"
-              alt=""
+              alt="This Sponsor is Community Foundation"
             />
           </div>
           <div className={styles.slide}>
             <img
-              src="https://i.ibb.co/fDL9QLz/brand-number-eleven.png"
+              src={bogues}
               height="100"
               width="250"
-              alt=""
+              alt="This Sponsor is Michael Bogues Construction"
             />
           </div>
           <div className={styles.slide}>
             <img
-              src="https://i.ibb.co/z5YCnWD/brand-number-twelve.png"
+              src={BnD}
               height="100"
               width="250"
-              alt=""
+              alt="This Sponsor is B &amp; D Controlled Air Corp"
             />
           </div>
           <div className={styles.slide}>
-            <img src="" height="100" width="250" alt="" />
+            <img
+              src={sentinel}
+              height="100"
+              width="250"
+              alt="This Sponsor is The Greenwich Sentinel"
+            />
+          </div>
+          <div className={styles.slide}>
+            <img
+              src={alliedRoof}
+              height="100"
+              width="250"
+              alt="This Sponsor is Allied Roofing &amp; Sheet Metal"
+            />
+          </div>
+          <div className={styles.slide}>
+            <img
+              src={lowesLogo}
+              alt="This Sponsor is Lowes"
+              height="100"
+              width="250"
+            />
+          </div>
+          <div className={styles.slide}>
+            <img
+              src={homeDepot}
+              height="100"
+              width="250"
+              alt="This Sponsor is Home Depot"
+            />
+          </div>
+          <div className={styles.slide}>
+            <img
+              src={searsLogo}
+              height="100"
+              width="250"
+              alt="This Sponsor is Sears"
+            />
+          </div>
+          <div className={styles.slide}>
+            <img
+              src={websterBank}
+              height="100"
+              width="250"
+              alt="This Sponsor is Webster Bank"
+            />
+          </div>
+          <div className={styles.slide}>
+            <img
+              src={boaLogo}
+              height="100"
+              width="250"
+              alt="This Sponsor is Bank of America"
+            />
+          </div>
+          <div className={styles.slide}>
+            <img
+              src={microsoftLogo}
+              height="100"
+              width="250"
+              alt="This Sponsor is Microsoft"
+            />
+          </div>
+          <div className={styles.slide}>
+            <img
+              src={commFoundation}
+              height="100"
+              width="250"
+              alt="This Sponsor is Community Foundation"
+            />
           </div>
         </div>
       </div>
