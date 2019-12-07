97e2465b83bd01e4f4cc889eda8634f6758a49fb
Cipher-Coder <matthew.rebehn@gmail.com>
2019-12-06 22:55:07 -0500

    added some pics | worked on contact form

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -3,6 +3,7 @@ import { navigate } from "gatsby-link"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import Header from "../components/Header"
+import keepCalm from "../images/keepCalm.png"
 import "../styles/volunteer-form.css"
 
 function encode(data) {
@@ -37,53 +38,60 @@ export default function Contact() {
     <Layout>
       <Seo title="Contact Us" />
       <Header pageHeading="Contact Us" />
-      <div className="container">
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:
-              <br />
-              <input type="text" name="name" onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:
-              <br />
-              <input
-                type="email"
-                name="email"
-                id="contact-email"
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:
-              <br />
-              <textarea name="message" onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+      <div className="container-fluid ml-4">
+        <div className="row col-sm-6">
+          <h1>Contact</h1>
+        </div>
+        <div className="row">
+          <form
+            name="contact"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your name:
+                <br />
+                <input type="text" name="name" onChange={handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your email:
+                <br />
+                <input
+                  type="email"
+                  name="email"
+                  id="contact-email"
+                  onChange={handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message:
+                <br />
+                <textarea name="message" onChange={handleChange} />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+          <div className="row pl-2">
+            <img src={keepCalm} alt="Keep Calm" className="col-sm-6" />
+          </div>
+        </div>
       </div>
     </Layout>
   )
