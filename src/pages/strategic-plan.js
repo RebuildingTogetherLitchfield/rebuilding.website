b291f5e8e95900fbc662d342b3a8e609d60a7c4a
Matt <matthew.rebehn@gmail.com>
2020-09-23 21:23:42 -0400

    Grammar in strategic-plan

--- src/pages/strategic-plan.js
+++ src/pages/strategic-plan.js
@@ -27,34 +27,34 @@ const StrategicPlan = () => {
             Rebuild Day, 4th weekend in April annually: individual sites worked
             at by teams from religious/civic/municipal or business groups. Work
             scheduled at sites only as funding permits each year. Increased
-            visibility in Litchfield County will provide increased # of
+            visibility in Litchfield County will provide an increase # of
             low-income, elderly homeowner referrals, and volunteer teams.
             Emphasis will be placed on community recognition.
           </p>
           <p className={styles.planText}>
-            There is a potential for creating smaller geographic zones with
+            There is the potential for creating smaller geographic zones with
             Board Members from each zone, or a group of zone advisors that would
             control travel distances for site assessments, etc.
           </p>
           <p className={styles.planText}>
             Homeowner (Maintenance) Training using Rebuilding Together (R T)
             manuals, volunteer and paid instructors. Trainings are in
-            cooperation with local Community groups, Senior Centers, Western
+            cooperation with local Community Groups, Senior Centers, Western
             Connecticut AAA and other organizations creating partnerships.
           </p>
           <ul className={styles.planTextList}>
-            <li>R T Homeowners Safe At Home Program</li>
+            <li>Rebuilding Together Homeowners Safe At Home Program</li>
             <li>
-              R T is responsible for construction budget, trainers and 'hands
-              on' equipment necessary using paid contract labor, when necessary,
-              who are reliable and can/will respond quickly & are known and
+              RT is responsible for construction budget, trainers and 'hands on'
+              equipment necessary using paid contract labor, when necessary, who
+              are reliable and can/will respond quickly & are known and
               therefore trusted to be referred to older Year-Round Emergency
               Maintenance homes persons
             </li>
-            <li>We would be familiar with their work</li>
-            <li>We would be sure that work was done in a timely way</li>
+            <li>We would be familiar with their work.</li>
+            <li>We would be sure that work was done in a timely manner.</li>
             <li>
-              Work would be billed without worry of inflated hours or supplies
+              Work would be billed without worry of inflated hours or supplies.
             </li>
           </ul>
           <p className={styles.planText}>
