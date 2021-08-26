365b8fe83dc1a32286cfbc39fc50e923108c8150
Matthew <matthew.rebehn@gmail.com>
2021-08-26 10:25:04 -0400

    Updated logo carousel to use data piped in from data and changed images to hosted ones

--- src/components/LogoCarousel.js
+++ src/components/LogoCarousel.js
@@ -1,167 +1,26 @@
 import React from "react"
 import styles from "./logocarousel.module.css"
-import lowesLogo from "../images/logos/lowesLogo1.jpg"
-import homeDepot from "../images/logos/homeDepot9.jpg"
-import searsLogo from "../images/logos/searsLogo1.jpg"
-import websterBank from "../images/logos/websterBank.jpg"
-import boaLogo from "../images/logos/boaLogo.jpg"
-import microsoftLogo from "../images/logos/microsoftLogo.jpg"
-import commFoundation from "../images/logos/commFoundation1.jpg"
-import bogues from "../images/logos/bogues1.jpg"
-import BnD from "../images/logos/BnDcontrol.jpg"
-import sentinel from "../images/logos/sentinel.jpg"
-import alliedRoof from "../images/logos/alliedRoof.jpg"
+import sliderImg from "../data/sliderImg"
 
 const LogoCarousel = () => {
+  const sliderImage = sliderImg.map((img) => {
+    return (
+      <div className={styles.slide}>
+        <img
+          key={img.name}
+          src={img.src}
+          alt={img.alt}
+          height="100"
+          width="250"
+        />
+      </div>
+    )
+  })
+
   return (
     <div className={`container-fluid ${styles.logoCarousel}`}>
       <div className={styles.slider}>
-        <div className={styles.slideTrack}>
-          <div className={styles.slide}>
-            <img
-              src={lowesLogo}
-              alt="This Sponsor is Lowes"
-              height="100"
-              width="250"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={homeDepot}
-              height="100"
-              width="250"
-              alt="This Sponsor is Home Depot"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={searsLogo}
-              height="100"
-              width="250"
-              alt="This Sponsor is Sears"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={websterBank}
-              height="100"
-              width="250"
-              alt="This Sponsor is Webster Bank"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={boaLogo}
-              height="100"
-              width="250"
-              alt="This Sponsor is Bank of America"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={microsoftLogo}
-              height="100"
-              width="250"
-              alt="This Sponsor is Microsoft"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={commFoundation}
-              height="100"
-              width="250"
-              alt="This Sponsor is Community Foundation"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={bogues}
-              height="100"
-              width="250"
-              alt="This Sponsor is Michael Bogues Construction"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={BnD}
-              height="100"
-              width="250"
-              alt="This Sponsor is B &amp; D Controlled Air Corp"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={sentinel}
-              height="100"
-              width="250"
-              alt="This Sponsor is The Greenwich Sentinel"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={alliedRoof}
-              height="100"
-              width="250"
-              alt="This Sponsor is Allied Roofing &amp; Sheet Metal"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={lowesLogo}
-              alt="This Sponsor is Lowes"
-              height="100"
-              width="250"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={homeDepot}
-              height="100"
-              width="250"
-              alt="This Sponsor is Home Depot"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={searsLogo}
-              height="100"
-              width="250"
-              alt="This Sponsor is Sears"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={websterBank}
-              height="100"
-              width="250"
-              alt="This Sponsor is Webster Bank"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={boaLogo}
-              height="100"
-              width="250"
-              alt="This Sponsor is Bank of America"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={microsoftLogo}
-              height="100"
-              width="250"
-              alt="This Sponsor is Microsoft"
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src={commFoundation}
-              height="100"
-              width="250"
-              alt="This Sponsor is Community Foundation"
-            />
-          </div>
-        </div>
+        <div className={styles.slideTrack}>{sliderImage}</div>
       </div>
     </div>
   )
---
+++ src/data/sliderImg.js
@@ -0,0 +1,70 @@
+const SLIDER_IMG = [
+  {
+    name: "Lowes",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629987036/RebuildingTogether/SponsorLogo/lowesLogo1_zw1qkp.jpg",
+    alt: "This sponsor is Lowes",
+  },
+  {
+    name: "Home Depot",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629986938/RebuildingTogether/SponsorLogo/homeDepot9_uxpuoz.jpg",
+    alt: "This sponsor is Home Depot",
+  },
+  {
+    name: "Sears",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629987016/RebuildingTogether/SponsorLogo/searsLogo1_ohlvo1.jpg",
+    alt: "This sponsor is Sears",
+  },
+  {
+    name: "Webster Bank",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629986992/RebuildingTogether/SponsorLogo/websterBank_sjdyg2.jpg",
+    alt: "This sponsor is Webster Bank",
+  },
+  {
+    name: "Bank of America",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629986938/RebuildingTogether/SponsorLogo/boaLogo_of7nfp.jpg",
+    alt: "This sponsor is Bank of America",
+  },
+  {
+    name: "Microsoft",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629987028/RebuildingTogether/SponsorLogo/microsoftLogo_kohl8a.jpg",
+    alt: "This sponsor is Microsoft",
+  },
+  {
+    name: "CT Community Foundation",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629986937/RebuildingTogether/SponsorLogo/commFoundation1_a7zlsk.jpg",
+    alt: "This sponsor is CT Community Foundation",
+  },
+  {
+    name: "Bogues Construction",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629986938/RebuildingTogether/SponsorLogo/bogues1_nfrjcu.jpg",
+    alt: "This sponsor is Bogues Construction",
+  },
+  {
+    name: "B & D Controlled Air Corp",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629986919/RebuildingTogether/SponsorLogo/BnDcontrol_ayi6vc.jpg",
+    alt: "This sponsor is B & D Controlled Air Corp",
+  },
+  {
+    name: "Greenwich Sentinel",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629987000/RebuildingTogether/SponsorLogo/sentinel_v8yphj.jpg",
+    alt: "This sponsor is Greenwich Sentinel",
+  },
+  {
+    name: "Allied Roofing",
+    src:
+      "https://res.cloudinary.com/mattaz/image/upload/v1629986906/RebuildingTogether/SponsorLogo/alliedRoof_y2kwgw.jpg",
+    alt: "This sponsor is Allied Roofing",
+  },
+]
+
+export default SLIDER_IMG
